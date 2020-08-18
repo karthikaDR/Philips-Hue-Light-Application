2b7e3dc35dc012fdda4526d65c118851c285cad8
KarthikaDR <44855227+karthikaDR@users.noreply.github.com>
2020-08-18 10:15:48 -0700

    Header Page

---
+++ Header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>lab2.php</title>
+        <style>
+        </style>
+    </head>
+    <body>
+    <h1>A php page to handle new user registration and existing user login </h1>
+    
+</html>
